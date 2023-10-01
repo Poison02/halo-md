88375edce3fa9abcd451a4a63c7d487b5f628b72
zchhhhh <2069820192@qq.com>
2023-10-01 16:26:12 +0800

    feat: 新增尚硅谷数据结构预算法/稀疏数组代码实现

---
+++ 算法/尚硅谷/data-structures/src/main/java/com/zch/sparsearray/SparseArray.java
@@ -0,0 +1,74 @@
+package com.zch.sparsearray;
+
+/**
+ * 稀疏数组代码实现
+ * 使用五子棋的示例表示
+ * @author Zch
+ * @date 2023/10/1
+ **/
+public class SparseArray {
+
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组 11 * 11
+        // 0 表示没有棋子，1表示黑子，2表示白子
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[4][5] = 2;
+        // 输出原始数组
+        System.out.println("原始的二维数组");
+        for (int[] row : chessArr1) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+        // 将二维数组转为稀疏数组
+        int sum = 0;
+        for (int i = 0; i < chessArr1.length; i++) {
+            for (int j = 0; j < chessArr1[0].length; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        int sparseArr[][] = new int[sum + 1][3];
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+        int count = 0;
+        for (int i = 0; i < chessArr1.length; i++) {
+            for (int j = 0; j < chessArr1[0].length; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++;
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+        // 输出稀疏数组
+        System.out.println("稀疏数组如下");
+        for (int[] row : sparseArr) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        // 将稀疏数组转为原始数组
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        // 输出原始数组
+        System.out.println("原始的二维数组");
+        for (int[] row : chessArr2) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+    }
+
+}
