55cd2f8c8ff3cfff4a66dc7299b9f418bf2194af
Poison02 <2069820192@qq.com>
2023-11-14 19:06:57 +0800

    feat：新增左程云算法新手班-day03

---
+++ 算法/左程云算法/新手班/code/src/cdu/zch/day03/BSExist.java
@@ -0,0 +1,65 @@
+package cdu.zch.day03;
+
+import java.util.Arrays;
+
+/**
+ * @author Poison02
+ * 二分查找
+ */
+public class BSExist {
+
+    public static boolean find(int[] arr, int num) {
+        if (arr == null || arr.length == 0) {
+            return false;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < num) {
+                left = mid + 1;
+            } else if (arr[mid] > num) {
+                right = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean test(int[] sortedArr, int num) {
+        for (int cur : sortedArr) {
+            if (cur == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) (Math.random() * (maxSize + 1))];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (test(arr, value) != find(arr, value)) {
+                System.out.println("出错了！");
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+
+}
